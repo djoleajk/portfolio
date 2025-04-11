73727a7eefbcb095c19c584cc37018ef7dd62e3e
Djordje Zivanovic <41837686+djoleajk@users.noreply.github.com>
2025-04-11 13:11:10 +0200

    Delete Portfolio directory

--- Portfolio/Sistem_za_rezervacije/templates/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<footer class="bg-light text-center py-3 mt-5">
-    <p>&copy; <?php echo date('Y'); ?> Reservation System. All rights reserved.</p>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- Portfolio/Sistem_za_rezervacije/templates/home.php
+++
@@ -1,9 +0,0 @@
-<?php include 'templates/header.php'; ?>
-
-<div class="container mt-5">
-    <h1>Dobrodošli u Sistem za Rezervacije</h1>
-    <p>Rezervišite svoje termine lako i upravljajte svojim rasporedom.</p>
-    <a href="index.php?page=reservations" class="btn btn-primary">Pogledaj Rezervacije</a>
-</div>
-
-<?php include 'templates/footer.php'; ?>
